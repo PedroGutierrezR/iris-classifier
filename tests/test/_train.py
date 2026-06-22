f602b60c63e5e54fe67204a5db6d8529f581b1e1
Pedro Gutierrez <pedrogutierrez@Faiths-Mac-mini.local>
2026-06-22 13:03:57 +0100

    update test: test_confusion_matrix_file

--- tests/test/_train.py
+++ tests/test/_train.py
@@ -7,8 +7,8 @@ class TestTrainAccuracy(unittest.TestCase):
     def test_accuracy(self):
         self.assertGreaterEqual(accuracy, 0.9)
 
-    def test_file(self):
-        path = Path(__file__).parent.parent.joinpath("../outputs/confusion/_matrix.png").resolve()
+    def test_confusion_matrix_file(self):
+        path = Path(__file__).parent.parent.joinpath("../outputs/confusion_matrix.png").resolve()
         self.assertTrue(path.is_file())
 
 if __name__ == '__main__':
